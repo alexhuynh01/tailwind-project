440b343d7bd3eb8eff04bd23e27c95946853f002
alexhuynh <121414952+alexhuynh01@users.noreply.github.com>
2024-02-23 21:10:29 +0700

    add subcribe us

--- public/main.js
+++ public/main.js
@@ -1,16 +1,26 @@
-const topMenu =  document.getElementById('alex-top-menu')
-const toggleTopMenu =  document.getElementById('alex-toggle-top-menu')
+const topMenu = document.getElementById("alex-top-menu");
+const toggleTopMenu = document.getElementById("alex-toggle-top-menu");
 
-
-document.addEventListener('click', (e) =>{
-  if(toggleTopMenu.contains(e.target)){
+document.addEventListener("click", (e) => {
+  if (toggleTopMenu.contains(e.target)) {
     //Click toggle top menu
-    topMenu.classList.toggle('alex-topmenu-expanded')
-    topMenu.classList.toggle('hidden')
-  }else if(topMenu.classList.contains('alex-topmenu-expanded')){
+    topMenu.classList.toggle("alex-topmenu-expanded");
+    topMenu.classList.toggle("hidden");
+  } else if (topMenu.classList.contains("alex-topmenu-expanded")) {
     //Click out side top menu
-    topMenu.classList.remove('alex-topmenu-expanded')
-    topMenu.classList.add('hidden')
+    topMenu.classList.remove("alex-topmenu-expanded");
+    topMenu.classList.add("hidden");
   }
+});
+window.onload = function () {
+  // Khôi phục vị trí cuộn từ sessionStorage, nếu có
+  var scrollPosition = sessionStorage.getItem("scrollPosition");
+  if (scrollPosition) {
+    window.scrollTo(0, scrollPosition);
+  }
+};
 
-})
+window.onbeforeunload = function () {
+  // Lưu vị trí cuộn vào sessionStorage trước khi trang tải lại hoặc đóng
+  sessionStorage.setItem("scrollPosition", window.scrollY);
+};
